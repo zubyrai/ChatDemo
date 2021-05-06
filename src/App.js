00d6d230ec7358e7bb61710421b630ecbb11d875
Zub7 <45486584+zubyrai@users.noreply.github.com>
2021-05-06 09:48:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import KommunicateChat from './chat';
+function App() {
+  return (
+    <div>
+   <h1>Welcome to Middlesex University</h1>
+      <KommunicateChat/>
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/chat.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+class KommunicateChat extends Component {
+    constructor(props){
+    super(props);
+}
+componentDidMount(){
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"29b0c80aa4ae3542edc67131b8cce22ae","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+}
+render() {
+    return (
+        <div></div>
+    )
+}
+}
+export default KommunicateChat;
